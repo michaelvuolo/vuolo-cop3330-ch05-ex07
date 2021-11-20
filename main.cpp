0c43d3374e4ab674a077ff265a96c9ccff3e1045
michaelvuolo <michaelvuolo1@gmail.com>
2021-11-20 18:57:19 -0500

    done

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// function prototype
+void calcRoots(double a, double b, double c);
+
+int main() {
+    // init variables
+	double a, b, c;
+
+    // read a, b, & c from user
+	cout << "Enter a, b, and c: ";
+	cin >> a >> b >> c;
+
+    // calculate and display root(s) (if real)
+    calcRoots(a, b, c);
+}
+
+void calcRoots(double a, double b, double c) {
+    // init variables used for calculation
+    double x1, x2, root;
+
+    // calculate root
+    root = pow(b, 2) - (4 * a * c);
+
+    // calculate & display roots
+	if (root == 0) {
+        x1 = -b + sqrt(root) / (2 * a);
+        cout << "The real root is " << x1 << ".";
+	}
+	else if (root > 0) {
+        x1 = (-b - sqrt(root)) / (2 * a);
+        x2 = (-b + sqrt(root)) / (2 * a);
+        cout << "The real roots are " << x1 << " or " << x2 << ".";
+	}
+	else
+        cout << "No real roots.";
+}
